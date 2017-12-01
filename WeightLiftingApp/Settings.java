c9e0931e91e65db3ce465a74eb61a589f91d25eb
Chris Lee <chlee@vassar.edu>
2017-11-30 20:49:39 -0500

    added settings and tester

---
+++ WeightLiftingApp/Settings.java
@@ -0,0 +1,39 @@
+
+public class Settings
+{
+    public int maxFS;
+    public int maxHC;
+    public int maxDL;
+    
+    public Settings()
+    {
+    }
+
+    public void setMaxFS(int maxFS)
+    {
+        this.maxFS = maxFS;
+    }
+    public int getMaxFS()
+    {
+        return maxFS;
+    }
+    
+    public void setMaxHC(int maxHC)
+    {
+        this.maxHC = maxHC;
+    }
+    public int getMaxHC()
+    {
+        return maxHC;
+    }
+    
+    public void setMaxDL(int maxDL)
+    {
+        this.maxDL = maxDL;
+    }
+    public int getMaxDL()
+    {
+        return maxDL;
+    }
+
+}
