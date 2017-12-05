d2fb1e39442dc2104cf88d671dd62b1e9cdecd31
Chris Lee <chlee@vassar.edu>
2017-12-05 01:48:13 -0500

    update

--- WeightLiftingApp/DayOne.java
+++ WeightLiftingApp/DayOne.java
@@ -25,20 +25,20 @@ public class DayOne extends Workout
     
     public void display()
     {
-        // int maxFS;
-        // Scanner in = new Scanner(System.in);
-        // PercentageCalculator percentage = new PercentageCalculator();
+        int maxFS;
+        Scanner in = new Scanner(System.in);
+        PercentageCalculator percentage = new PercentageCalculator();
         
-        // System.out.println("Enter your max front squat weight: ");
-        // maxFS = in.nextInt();
-        // System.out.println("Here is your workout.");
-        // lowerBody.primaryLowerBody(maxFS,week);
-        // U1.upper1(week);
-        // S1.subset1();
-        // U2.upper2();
-        // S2.subset2();
-        // System.out.println("4A. DEADBUGS: 2x10ea");
-        // System.out.println("4B. BAND ANTIROTATION: 2x10ea");
+        System.out.println("Enter your max front squat weight: ");
+        maxFS = in.nextInt();
+        System.out.println("Here is your workout.");
+        lowerBody.primaryLowerBody(maxFS,week);
+        U1.upper1(week);
+        S1.subset1();
+        U2.upper2();
+        S2.subset2();
+        System.out.println("4A. DEADBUGS: 2x10ea");
+        System.out.println("4B. BAND ANTIROTATION: 2x10ea");
                   
         screen = new JFrame();
         screen.setSize(500,500);
