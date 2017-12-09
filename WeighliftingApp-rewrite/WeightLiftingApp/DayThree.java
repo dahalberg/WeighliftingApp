75c3a093900e7a26fb44d909e5d0e521ac209cc1
dahalberg <31749773+dahalberg@users.noreply.github.com>
2017-12-09 12:01:00 -0500

    Add files via upload
    Updated Day Three display method

--- WeighliftingApp-rewrite/WeightLiftingApp/DayThree.java
+++ WeighliftingApp-rewrite/WeightLiftingApp/DayThree.java
@@ -1,7 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class DayThree extends Workout
 {
-
+    private JFrame screen;
     public DayThree(int week)
     {
         super(week);
@@ -9,6 +12,16 @@ public class DayThree extends Workout
 
     public void display()
     {
-        
+        screen = new JFrame();
+        int maxRDL;
+        screen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        int xStart[]={200,400,400,200};
+        int yStart[]={450,450,550,550};
+        screen.setVisible(true);
+        // Not sure if this will go line by line yet
+        JLabel label = new JLabel("Enter your max RDL weight: ");
+        screen.add(label);
+        label.setText("Here is your workout");
+        // Change workout and place exercises here
     }
 }
