e2138a0d4dbc9e6b12bbadaceca2bba40b21a7a9
dahalberg <31749773+dahalberg@users.noreply.github.com>
2017-12-09 11:56:13 -0500

    Add files via upload
    Updated Day Two display method

--- WeighliftingApp-rewrite/WeightLiftingApp/DayTwo.java
+++ WeighliftingApp-rewrite/WeightLiftingApp/DayTwo.java
@@ -1,6 +1,10 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class DayTwo extends Workout
 {
+    private JFrame screen;
     public DayTwo(int week)
     {
         super(week);
@@ -8,6 +12,17 @@ public class DayTwo extends Workout
     
     public void display()
     {
-        
+        screen = new JFrame();
+        int maxFS;
+        int maxDB;
+        screen.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        int xStart[]={200,400,400,200};
+        int yStart[]={450,450,550,550};
+        screen.setVisible(true);
+        // Not sure if this will go line by line yet
+        JLabel label = new JLabel("Enter your max hang clean weight: ");
+        screen.add(label);
+        label.setText("Here is your workout");
+        // Change workout and place exercises here
     }
 }
